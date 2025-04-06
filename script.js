efb3d4f5cf4b23948ac7460992d89e07290cb6be
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-04-05 20:42:39 -0400

    feat: add dynamic box sizing and grid size validation
    - Calculate and apply dynamic box size based on grid size
    - Remove hardcoded CSS for height and width
    - Restrict grid size to a maximum of 100

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function createGrid(size) {
 
   let rows = size;
   let columns = size;
+  let boxSize = 960/size;
 
   for (let i = 0; i < rows; i++ ) {
     const row = document.createElement("div");
@@ -14,6 +15,7 @@ function createGrid(size) {
       const box = document.createElement("div")
       box.classList.add("box")
       row.appendChild(box);
+      box.style.cssText = `height: ${boxSize}px; width: ${boxSize}px;`
       box.addEventListener("mouseenter", () => {
         box.classList.add("active")
       });
@@ -28,5 +30,24 @@ createGrid(16);
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
   let newSize = prompt("Enter grid size");
-  createGrid(newSize);
-})
+  if (newSize > 100) {
+    alert('Max grid size is 100. Setting grid size to 100...');
+    newSize = 100;
+    createGrid(100);
+  } else {
+    createGrid(newSize);
+  }
+});
+
+/*btn.addEventListener("click", () => {
+  let newSize = prompt("Enter grid size");
+  if (newSize > 100) {
+    const para = document.createElement("p");
+    para.innerText = 'Max grid size is 100. Setting grid size to 100...'
+    btn.after(para); 
+    newSize = 100;
+    createGrid(100);
+  } else {
+    createGrid(newSize);
+  }
+}*/
